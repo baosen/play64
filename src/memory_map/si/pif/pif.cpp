008b774f1da471de43de8f1a7eda466d0622d1ab
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-05-10 19:38:33 +0200

    Docs.

--- src/memory_map/si/pif/pif.cpp
+++ src/memory_map/si/pif/pif.cpp
@@ -2,7 +2,6 @@
 #include "pif.hpp"
 #include "../../mem.hpp"
 #include <iostream>
-using namespace std;
 
 namespace {
     u8 pifmem[JOYCHANNEL_RAM_END-BOOT_ROM_START];
@@ -10,12 +9,17 @@ namespace {
 
 // The PIF RAM is used to write command blocks that commands the PIF to save values back to the PIF RAM.
 namespace Pif {
-    void reset() { zero(pifmem); }
+    void reset() {
+        zero(pifmem);
+    }
+
+    // Set PIF random access memory.
     void set(const u32 i, const u32 val) {
         pifmem[i] = val;
-        switch (pifmem[0x7fc]) { // Interface to the security chip.
+
+        switch (pifmem[0x7fc]) { // Memory interface to the security chip.
         case 8: // Needs to write this value to this address or the N64 will lock.
-            cout << "[PIF] Access granted!" << endl;
+            std::cout << "[PIF] Access granted!" << std::endl;
             break;
         }
     }
