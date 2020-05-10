3d45f65ded9b28e648cb9905520f43ee227fe790
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-05-10 19:48:24 +0200

    Cleanup.

--- src/memory_map/vi/framebuffer.cpp
+++ src/memory_map/vi/framebuffer.cpp
@@ -12,7 +12,3 @@ void framebuffer::set_framebuffer_address(memory_address memory_address_to_set) 
 memory_address framebuffer::get_framebuffer_address() const {
     return m_memory_address;
 }
-
-void framebuffer::set_main_window(wxWindow *ptr_to_main_window) {
-    main_window = ptr_to_main_window;
-}
