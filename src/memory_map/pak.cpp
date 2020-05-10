40bb7452255fb4e160cd830782feeab416f46e09
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-05-10 14:18:03 +0200

    Docs.

--- src/memory_map/pak.cpp
+++ src/memory_map/pak.cpp
@@ -43,32 +43,41 @@ namespace Pak {
     void load(const char path[]) {
         // Is not nullptr?
         assert(path);
+
         // Open ROM.
         const auto fp = fopen(path, "rb");
         if (!fp)
     		throw err(string("Failed to open file: ") + path);
         const auto l = len(fp, path);
+
         // Check if ROM is too large.
         if (l > DOMAIN_SIZE)
             throw err("ROM file is too large.");
+
         // Load ROM into the memory.
         if (fread(rom, 1, l, fp) <= 0) {
             close(fp, path);
     		throw err(string("Failed to read file: ") + path);
         }
         close(fp, path);
+
     	// Byteswap the entire rom to the correct endianness for the ROM in the Game Pak.
     	rombswap(rom, l);
+
         // Load bootcode to SP DMEM.
         memcpy(Sp::dmem+0x40, rom+0x40, 0xfc0);
+
         // Set TV-type.
         Physmem::wr32(0x300, NTSC);
+
         // Reset interpreter's state.
         Interpreter::reset();
-        // Cart is loaded!
+
+        // Set cart is loaded!
         loaded = true;
     }
-    
+
+    // Read 32-bit value.
     u32 rd32(const u32 i) {
     	if (i > sizeof(rom)-1)
             throw err("Outside ROM!");
