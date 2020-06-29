2f454a35cd052cfdd984466583d11772b3c970e9
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-06-29 16:55:26 +0200

    Docs.

--- src/memory_map/physical_memory/vi/vi.cpp
+++ src/memory_map/physical_memory/vi/vi.cpp
@@ -26,7 +26,7 @@ namespace Vi {
         ANTIALIAS    = 0x300
     };
 
-// Controls the behaviour of the Video DAC/Encoder:
+    // Controls the behaviour of the Video DAC/Encoder:
     WR(control) { // Sets the pixel-format of the framebuffer and how the pixels should be drawn.
         printf("[VI] Pixel size: %u bytes. Gamma-dither: %s. Divot: %s. Serrate: %s. Anti-alias: %s\n",
                 val & PIXEL_SIZE, 
@@ -37,92 +37,92 @@ namespace Vi {
         control = val;
     }
 
-// Framebuffer origin DRAM address:
+    // Framebuffer origin DRAM address:
     RD(origin) {
         return fbaddr;
     }
 
-// Sets the memory address of the currently used framebuffer.
-// Usually used to implement double buffering.
+    // Sets the memory address of the currently used framebuffer.
+    // Can (and usually) used to implement double buffering.
     WR(origin) { // Framebuffer physical memory address that points to the origin.
         fbaddr = val;
         printf("[VI] Framebuffer-address: %08x\n", val);
     }
 
-// Sets the width of the framebuffer in pixels.
+    // Sets the width of the framebuffer in pixels.
     WR(width) { // horizontal width.
         printf("[VI] Framebuffer-width: %u pixels\n", val);
     }
 
-// Assert an interrupt in combination with the MI when the Video DAC hits a certain line.
+    // Assert an interrupt in combination with the MI when the Video DAC hits a certain line.
     WR(intr) { // interrupt.
         line = val; // on which line to interrupt.
     }
 
-// Read the current half line the video DAC is working on.
+    // Read the current half line the video DAC is working on.
     RD(current) {
         printf("[VI] Read current half line: %d\n", line);
         return line;
     }
 
-// Clears asserted VI interrupt when written.
+    // Clears asserted VI interrupt when written.
     WR(current) { // current vertical half line.
         Mi::clearVI();
     } 
 
-// VI video timing.
+    // VI video timing.
     WR(timing) {
     }
-// [7:0] horizontal sync width in pixels
-// [15:8] color burst width in pixels
-// [19:16] vertical sync width in half lines
-// [29:20] start of color burst in pixels from h-sync
+    // [7:0] horizontal sync width in pixels
+    // [15:8] color burst width in pixels
+    // [19:16] vertical sync width in half lines
+    // [29:20] start of color burst in pixels from h-sync
 
-// Number of lines per frame in NTSC. Number of half-lines per field in PAL.
+    // Number of lines per frame in NTSC. Number of half-lines per field in PAL.
     WR(vsync) { 
         line = val;
         printf("[VI] Vertical sync set: %d lines\n", val);
     }
 
-// Number of horizontal lines before horizontal retrace?
+    // Number of horizontal lines before horizontal retrace?
 	WR(hsync) {
         printf("[VI] Horizontal sync set: %d pixels\n", val);
     }
 
-// Horizontal sync leap.
+    // Horizontal sync leap.
 	WR(hsync_leap) { // wtf?
     }
 
-// Horizontal video start.
+    // Horizontal video start.
 	WR(hstart) { // wtf?
         printf("[VI] Horizontal video start: %d pixel end: %d pixel\n", val >> 15, (val & 0x1ff));
     }
-// (RW): [9:0] end of active video in screen pixels
-//       [25:16] start of active video in screen pixels
+    // (RW): [9:0] end of active video in screen pixels
+    //       [25:16] start of active video in screen pixels
     
 
-// Vertical video start.
+    // Vertical video start.
 	WR(vstart) { 
         printf("[VI] Vertical video start: %d line end: %d line\n", val >> 15, (val & 0x1ff));
     }
-// [9:0] end of active video in screen lines.
-// [25:16] start of active video in screen lines.
+    // [9:0] end of active video in screen lines.
+    // [25:16] start of active video in screen lines.
 
-// Vertical burst.
+    // Vertical burst.
 	WR(vburst) {
     }
-// [9:0] end of color burst enable in half-lines.
-// [25:16] start of color burst enable in half-lines.
+    // [9:0] end of color burst enable in half-lines.
+    // [25:16] start of color burst enable in half-lines.
 
-// Horizontal (X) upscale.
+    // Horizontal (X) upscale.
 	WR(xscale) {
         xscale = val;
     }
-// Allows scaling the image up horizontally (in the X direction) to the required display format from a reduced number of rendered pixels per line
+    // Allows scaling the image up horizontally (in the X direction) to the required display format from a reduced number of rendered pixels per line
 
-// Vertical (Y) upscale.
+    // Vertical (Y) upscale.
 	WR(yscale) {
         yscale = val;
     }
-// Allows scaling the image up vertically (in the Y direction) to the required display format from a reduced number of rendered lines per frame
+    // Allows scaling the image up vertically (in the Y direction) to the required display format from a reduced number of rendered lines per frame
 }
--- src/memory_map/physical_memory/vi/vi.hpp
+++ src/memory_map/physical_memory/vi/vi.hpp
@@ -3,20 +3,36 @@
 
 // Video interface.
 namespace Vi {
+    // Controls the video output.
     WR(control);
+
+    // The physical memory address to an allocated framebuffer.
     RD(origin);
     WR(origin);
+
+    // The width of the framebuffer in pixels. How big is a pixel?
     WR(width);
+
     WR(intr);
+
     RD(current);
     WR(current);
+
     WR(timing);
+
     WR(vsync);
+
 	WR(hsync);
+
 	WR(hsync_leap);
+
 	WR(hstart);
+
 	WR(vstart);
+
 	WR(vburst);
+
 	WR(xscale);
+
 	WR(yscale);
 };
