f38edbfbf27c179bf26ec2978cf9eb46dc735155
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-06-30 10:18:51 +0200

    Abstracted.

--- src/memory_map/physical_memory/vi/control.cpp
+++ src/memory_map/physical_memory/vi/control.cpp
@@ -32,34 +32,34 @@ enum {
 
 namespace vi { namespace control {
     unsigned int get_tv_type() {
-        return ::control & 3;
+        return vi::control::get() & 3;
     }
 
     // Check if gamma dither video output filter has been enabled by the game.
     bool is_gamma_dither_enabled() {
-        return ::control >> 2 & 1;
+        return vi::control::get() >> 2 & 1;
     }
 
     // Check if gamma video output filter has been enabled by the game.
     bool is_gamma_enabled() {
-        return ::control >> 3 & 1;
+        return vi::control::get() >> 3 & 1;
     }
 
     // Check if divot video output filter has been enabled by the game.
     bool is_divot_enabled() {
-        return ::control >> 4 & 1;
+        return vi::control::get() >> 4 & 1;
     }
 
     // Check if game wants to enable the vbus clock.
     // This flag was introduced and used only on Ultra 64 prototypes.
     // Enabling it on the final hardware may permanently damage it.
     bool is_vbus_clock_enabled() {
-        return ::control >> 5 & 1;
+        return vi::control::get() >> 5 & 1;
     }
 
     // Enable serrate filter on video output.
     bool serrate() {
-        return ::control >> 6 & 1;
+        return vi::control::get() >> 6 & 1;
     }
 
     void set(int32_t value) {
