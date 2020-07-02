6af859385452cc42fbf7c9f45e3733fe7195edc3
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-07-02 11:25:35 +0200

    Docs.

--- src/gui/sfml/window.cpp
+++ src/gui/sfml/window.cpp
@@ -6,14 +6,14 @@
 namespace {
     std::unique_ptr<sf::RenderWindow> window;
 
-    // Clear window.
-    void clear_window() {
+    // Clear the main window.
+    void clear_main_window() {
         assert(window->isOpen());
         window->clear();
     }
 
     // Display what is drawn in the framebuffer.
-    void display() {
+    void display_framebuffer() {
         assert(window->isOpen());
         window->display();
     }
@@ -26,11 +26,11 @@ namespace gui { namespace sfml {
 
     void output_16bit_image(char framebuffer[]) {
         assert(window != nullptr);
-        clear_window();
+        clear_main_window();
     }
 
     void output_32bit_image(char framebuffer[]) {
         assert(window != nullptr);
-        clear_window();
+        clear_main_window();
     }
 }}
