ee1c52c51f85020d321775b665900ba50deee952
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2020-07-07 10:39:01 +0200

    Removed unused code. Documented.

--- src/disassembler/decode.cpp
+++ src/disassembler/decode.cpp
@@ -3,46 +3,38 @@
 #include <cpu/interpreter/normals/regimms/regimmsstr.hpp>
 #include <cpu/interpreter/normals/cop1/cop1str.hpp>
 #include <cpu/interpreter/normals/cop0/cop0str.hpp>
-using namespace std;
-
-// Decode instruction returning its string in high-level syntax.
-string hdec(const Instr i) {
-    // Check if it is a "No operation"-instruction.
-    if (i.is_nop())
-        return "nop";
-    // Decode SPECIAL-opcodes (opcodes that has the SPECIAL-bits 0000000 in its normal opcode-field).
-    if (i.is_special())
-    // Decode Register-Immediate instructions.
-    if (i.is_regimm())
-    // System control processor.
-    if (i.is_cop0())
-    // Decode Coprocessor 1-instructions, which is the floating point-coprocessor:
-    if (i.is_cop1())
-    // RSP's vector instructions.
-    if (i.is_cop2())
-        return "Unknown COP2-instruction!";
-    return build_normal(i);
-}
 
 // Decode an MIPS instruction. Return the decoded instruction as a string.
-string decode(const Instr i) {
+std::string decode(const Instr instruction_to_decode) {
 	// Check if it is a "No operation"-instruction.
-	if (i.is_nop())
+	if (instruction_to_decode.is_nop()) {
 		return "nop";
+	}
+
 	// Decode SPECIAL-opcodes (opcodes that has the SPECIAL-bits 0000000 in its normal opcode-field).
-	if (i.is_special())
-		return build_special(i);
+	if (instruction_to_decode.is_special()) {
+		return build_special(instruction_to_decode);
+	}
+
 	// Decode Register-Immediate instructions.
-	if (i.is_regimm())
-		return build_regimm(i);
+	if (instruction_to_decode.is_regimm()) {
+		return build_regimm(instruction_to_decode);
+	}
+
 	// System control processor.
-	if (i.is_cop0())
-		return build_cop0(i);
+	if (instruction_to_decode.is_cop0()) {
+		return build_cop0(instruction_to_decode);
+	}
+
 	// Decode Coprocessor 1-instructions, which is the floating point-coprocessor:
-	if (i.is_cop1())
-		return build_cop1(i);
+	if (instruction_to_decode.is_cop1()) {
+		return build_cop1(instruction_to_decode);
+	}
+
 	// RSP's vector instructions.
-	if (i.is_cop2())
+	if (instruction_to_decode.is_cop2()) {
 		return "Unknown COP2-instruction!";
-	return build_normal(i);
+	}
+
+	return build_normal(instruction_to_decode);
 }
